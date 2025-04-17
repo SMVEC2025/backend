79c089f6728b9bac2d373f6c7a75c4ff987db03e
premkumar <premkumarananthan1@gmail.com>
2025-04-17 12:06:41 +0530

    changes

--- src/pages/api/accept-chat.js
+++ src/pages/api/accept-chat.js
@@ -11,22 +11,15 @@ const allowedOrigins = [
   '*'
 ];
 
-
-
 export default async function handler(req, res) {
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    return res.status(200).end(); // handle preflight
   }
+
 
   if (req.method === 'POST') {
 
